6ebd54ca99a4507b3a31436c049432650463ad1b
rekha <rekhaelizabethmathew21@gmail.com>
2024-11-12 15:11:42 +0530

    Initial commit

---
+++ gallery/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Photo(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="photos/")
+    thumbnail = models.ImageField(upload_to="thumbnails/", null=True)
+
+    def __str__(self):
+        return str(self.title)
+
---
+++ gallery/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render, redirect
+from .forms import PhotoForm
+from .models import Photo
+from .tasks import send_welcome_email_async, generate_thumbnail
+
+
+def gallery_view(request):
+    photos = Photo.objects.all()
+    return render(request, "gallery/gallery.html", {"photos": photos})
+
+
+def upload_photo(request):
+    if request.method == "POST":
+        form = PhotoForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            send_welcome_email_async.delay()
+            return redirect("gallery")
+    else:
+        form = PhotoForm()
+    return render(request, "gallery/upload.html", {"form": form})
+
+
+def gallery_view(request):
+    photos = Photo.objects.all()
+    return render(request, "gallery/gallery.html", {"photos": photos})
+ 
+ 
+def upload_photo(request):
+    if request.method == "POST":
+        form = PhotoForm(request.POST, request.FILES)
+        if form.is_valid():
+            photo = form.save()
+            generate_thumbnail.delay(photo.id)
+            send_welcome_email_async.delay()
+            return redirect("gallery")
+    else:
+        form = PhotoForm()
+    return render(request, "gallery/upload.html", {"form": form})
+ 
